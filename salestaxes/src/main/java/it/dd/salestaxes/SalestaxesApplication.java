044aa6aaad57164b85d58fd7e33866792943c5d8
daniele diodati <danidi82@gmail.com>
2018-02-05 21:56:13 +0100

    first commit

---
+++ salestaxes/src/main/java/it/dd/salestaxes/SalestaxesApplication.java
@@ -0,0 +1,12 @@
+package it.dd.salestaxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalestaxesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalestaxesApplication.class, args);
+	}
+}
